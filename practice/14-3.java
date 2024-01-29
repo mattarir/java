9131b4e4be2f9d17a40bd0a5ddedaba57e26db17
simada <ktq2390305@stu.o-hara.ac.jp>
2024-01-29 14:46:57 +0900

    Commit

--- practice/14-3.java
+++ practice/14-3.java
@@ -8,21 +8,21 @@ class Mondai36 {
 
         // ユーザーに文字列の入力を促す
         System.out.print("文字列を入力：");
-        // ユーザーが入力した文字列を取得
+        // ユーザーが入力した文字列を取得する
         String inputString = scanner.nextLine();
 
-        // 結果の文字列を生成
+        // 結果の文字列を生成する
         String resultString = manipulateString(inputString);
 
-        // 結果の文字列を画面に表示
+        // 結果の文字列を画面に表示する
         System.out.println("結果文字列：" + resultString);
     }
 
     private static String manipulateString(String inputString) {
-        // 文字列の長さが11以上の場合、11文字以降を削除
+        // 文字列の長さが11以上の場合、11文字以降を削除する
         if (inputString.length() >= 11) {
             StringBuilder stringBuilder = new StringBuilder(inputString);
-            stringBuilder.setLength(10); // 11文字以降を削除
+            stringBuilder.setLength(10); 
             return stringBuilder.toString();
         }
         // 文字列の長さが10文字以下はそのまま返す
